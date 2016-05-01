3c8ccc8e5bb0154d4c6b4b2bbd2f8d5f5478ad3a
cattaka <cattaka@mail.cattaka.net>
2016-05-02 00:09:56 +0900

    Add TestUtils

---
+++ app/src/androidTest/java/net/cattaka/android/snippets/test/ScrollToExAction.java
@@ -0,0 +1,65 @@
+package net.cattaka.android.snippets.test;
+
+import android.graphics.Rect;
+import android.support.test.espresso.PerformException;
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.espresso.util.HumanReadables;
+import android.support.v4.widget.NestedScrollView;
+import android.util.Log;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
+
+import org.hamcrest.Matcher;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+
+/**
+ * Created by takao on 2016/04/28.
+ */
+public class ScrollToExAction implements ViewAction {
+    private static final String TAG = ScrollToExAction.class.getSimpleName();
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Matcher<View> getConstraints() {
+        return allOf(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE), isDescendantOfA(anyOf(
+                isAssignableFrom(ScrollView.class), isAssignableFrom(HorizontalScrollView.class),
+                isAssignableFrom(NestedScrollView.class)
+        )));
+    }
+
+    @Override
+    public void perform(UiController uiController, View view) {
+        if (isDisplayingAtLeast(90).matches(view)) {
+            Log.i(TAG, "View is already displayed. Returning.");
+            return;
+        }
+        Rect rect = new Rect();
+        view.getDrawingRect(rect);
+        if (!view.requestRectangleOnScreen(rect, true /* immediate */)) {
+            Log.w(TAG, "Scrolling to view was requested, but none of the parents scrolled.");
+        }
+        uiController.loopMainThreadUntilIdle();
+        if (!isDisplayingAtLeast(90).matches(view)) {
+            throw new PerformException.Builder()
+                    .withActionDescription(this.getDescription())
+                    .withViewDescription(HumanReadables.describe(view))
+                    .withCause(new RuntimeException(
+                            "Scrolling to view was attempted, but the view is not displayed"))
+                    .build();
+        }
+    }
+
+    @Override
+    public String getDescription() {
+        return "scroll to";
+    }
+}
