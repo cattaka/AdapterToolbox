c747b452141342b418ad1fc7f91fbda3087dd854
cattaka <cattaka@mail.cattaka.net>
2016-05-01 23:31:16 +0900

    Initial import

---
+++ app/src/main/java/net/cattaka/android/snippets/adapter/ICodeLabel.java
@@ -0,0 +1,12 @@
+package net.cattaka.android.snippets.adapter;
+
+import android.content.res.Resources;
+
+/**
+ * Created by cattaka on 16/05/01.
+ */
+public interface ICodeLabel {
+    String getCode();
+
+    String getLabel(Resources res);
+}
