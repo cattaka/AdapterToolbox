09cb10d21b27e12464ab8c62d191063eee0e1110
cattaka <cattaka@mail.cattaka.net>
2016-05-05 00:02:24 +0900

    Add license of MergeRecyclerAdapter

--- snippets/src/main/java/net/cattaka/android/snippets/adapter/MergeRecyclerAdapter.java
+++ snippets/src/main/java/net/cattaka/android/snippets/adapter/MergeRecyclerAdapter.java
@@ -10,9 +10,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
+/*
  * forked from https://gist.github.com/athornz/008edacd1d3b2f1e1836
+ *
+ * Copyright 2016 Takao Sumitomo
+ * Copyright 2014 Josh Burton
  * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * The MergeRecyclerAdapter is inspired by the cwac-merge adapter, but for
  * the RecyclerView.
  * <p/>
