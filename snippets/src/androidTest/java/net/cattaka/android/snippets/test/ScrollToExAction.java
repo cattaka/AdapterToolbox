170a6438def5104a8b6937d1c3419c504df7177e
Takao Sumitomo <cattaka@mail.cattaka.net>
2016-05-11 22:27:54 +0900

    Update about JUnit tests

--- snippets/src/androidTest/java/net/cattaka/android/snippets/test/ScrollToExAction.java
+++ snippets/src/androidTest/java/net/cattaka/android/snippets/test/ScrollToExAction.java
@@ -21,6 +21,23 @@ import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVi
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anyOf;
 
+/*
+ * Copyright (C) 2016 Takao Sumitomo
+ * Copyright (C) 2014 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 /**
  * Created by takao on 2016/04/28.
  */
