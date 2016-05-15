695b89324c2b94dcecb7362e5a0dc47af4778d83
cattaka <cattaka@mail.cattaka.net>
2016-05-15 21:04:20 +0900

    Upgrade ForwardingListener

--- snippets/src/main/java/net/cattaka/android/snippets/adapter/listener/ListenerRelay.java
+++ snippets/src/main/java/net/cattaka/android/snippets/adapter/listener/ListenerRelay.java
@@ -1,17 +1,78 @@
 package net.cattaka.android.snippets.adapter.listener;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 /**
  * Created by cattaka on 16/05/14.
  */
 public class ListenerRelay<A extends RecyclerView.Adapter<? extends VH>, VH extends RecyclerView.ViewHolder> implements IListenerRelay<VH> {
-    public void onItemClick(RecyclerView parent, A adapter, int position, int id, VH vh) {
+    /**
+     * @see android.view.View.OnClickListener
+     */
+    public void onClick(RecyclerView recyclerView, A adapter, VH vh, View view) {
+    }
+
+    /**
+     * @see android.view.View.OnLongClickListener
+     */
+    public boolean onLongClick(RecyclerView recyclerView, A adapter, VH vh, View view) {
+        return false;
+    }
+
+    /**
+     * @see RadioGroup.OnCheckedChangeListener
+     */
+    public void onCheckedChanged(RecyclerView recyclerView, A adapter, VH vh, RadioGroup group, int checkedId) {
+    }
+
+    /**
+     * @see android.widget.CompoundButton.OnCheckedChangeListener
+     */
+    public void onCheckedChanged(RecyclerView recyclerView, A adapter, VH vh, CompoundButton buttonView, boolean isChecked) {
+    }
+
+    /**
+     * @see android.widget.SeekBar.OnSeekBarChangeListener
+     */
+    public void onProgressChanged(RecyclerView recyclerView, A adapter, VH vh, SeekBar seekBar, int progress, boolean fromUser) {
+    }
+
+    /**
+     * @see android.widget.SeekBar.OnSeekBarChangeListener
+     */
+    public void onStartTrackingTouch(RecyclerView recyclerView, A adapter, VH vh, SeekBar seekBar) {
+    }
+
+    /**
+     * @see android.widget.SeekBar.OnSeekBarChangeListener
+     */
+    public void onStopTrackingTouch(RecyclerView recyclerView, A adapter, VH vh, SeekBar seekBar) {
+    }
+
+    /**
+     * @see android.widget.AdapterView.OnItemSelectedListener
+     */
+    public void onNothingSelected(RecyclerView recyclerView, A adapter, VH vh, AdapterView<?> parent) {
 
     }
 
-    public boolean onItemLongClick(RecyclerView parent, A adapter, int position, int id, View view, VH vh) {
+    /**
+     * @see android.widget.AdapterView.OnItemSelectedListener
+     */
+    public void onItemSelected(RecyclerView recyclerView, A adapter, VH vh, AdapterView<?> parent, View view, int position, long id) {
+    }
+
+    /**
+     * @see android.widget.TextView.OnEditorActionListener
+     */
+    public boolean onEditorAction(RecyclerView recyclerView, A adapter, VH vh, TextView v, int actionId, KeyEvent event) {
         return false;
     }
 }
