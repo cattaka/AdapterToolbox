5432650823fbf2cc49c204f261db4e597e939c59
cattaka <cattaka@mail.cattaka.net>
2016-05-16 00:06:38 +0900

    Add TextInfoViewHolderFactory

--- example/src/main/java/net/cattaka/android/snippets/example/OperatableListExampleActivity.java
+++ example/src/main/java/net/cattaka/android/snippets/example/OperatableListExampleActivity.java
@@ -1,20 +1,26 @@
 package net.cattaka.android.snippets.example;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 import net.cattaka.android.snippets.adapter.ScrambleAdapter;
 import net.cattaka.android.snippets.adapter.listener.ListenerRelay;
 import net.cattaka.android.snippets.example.adapter.SimpleStringAdapter;
 import net.cattaka.android.snippets.example.adapter.factory.MyInfoViewHolderFactory;
+import net.cattaka.android.snippets.example.adapter.factory.TextInfoViewHolderFactory;
 import net.cattaka.android.snippets.example.data.MyInfo;
 import net.cattaka.android.snippets.example.data.OrdinalLabel;
+import net.cattaka.android.snippets.example.data.TextInfo;
 import net.cattaka.android.snippets.thirdparty.MergeRecyclerAdapter;
 
 import java.util.ArrayList;
@@ -30,8 +36,10 @@ public class OperatableListExampleActivity extends AppCompatActivity {
         public void onCheckedChanged(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, CompoundButton buttonView, boolean isChecked) {
             MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
             if (la.mAdapter == mItemAdapter) {
-                MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
-                item.setChecked(isChecked);
+                if (viewHolder instanceof MyInfoViewHolderFactory.ViewHolder) {
+                    MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
+                    item.setChecked(isChecked);
+                }
             }
         }
 
@@ -39,8 +47,10 @@ public class OperatableListExampleActivity extends AppCompatActivity {
         public void onProgressChanged(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, SeekBar seekBar, int progress, boolean fromUser) {
             MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
             if (la.mAdapter == mItemAdapter) {
-                MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
-                item.setIntValue(progress);
+                if (viewHolder instanceof MyInfoViewHolderFactory.ViewHolder) {
+                    MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
+                    item.setIntValue(progress);
+                }
             }
         }
 
@@ -48,8 +58,44 @@ public class OperatableListExampleActivity extends AppCompatActivity {
         public void onItemSelected(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, AdapterView<?> parent, View view, int position, long id) {
             MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
             if (la.mAdapter == mItemAdapter) {
-                MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
-                item.setOrdinalLabel((OrdinalLabel) parent.getItemAtPosition(position));
+                if (viewHolder instanceof MyInfoViewHolderFactory.ViewHolder) {
+                    MyInfo item = (MyInfo) mItemAdapter.getItemAt(la.mLocalPosition);
+                    item.setOrdinalLabel((OrdinalLabel) parent.getItemAtPosition(position));
+                }
+            }
+        }
+
+        @Override
+        public void afterTextChanged(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, TextView v, Editable s) {
+            MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
+            if (la.mAdapter == mItemAdapter) {
+                if (viewHolder instanceof TextInfoViewHolderFactory.ViewHolder) {
+                    TextInfo item = (TextInfo) mItemAdapter.getItemAt(la.mLocalPosition);
+                    item.setText(String.valueOf(s));
+                }
+            }
+        }
+
+        @Override
+        public boolean onEditorAction(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, TextView v, int actionId, KeyEvent event) {
+            MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
+            if (la.mAdapter == mItemAdapter) {
+                if (viewHolder instanceof TextInfoViewHolderFactory.ViewHolder) {
+                    Snackbar.make(v, "Action button pressed : " + v.getText(), Snackbar.LENGTH_SHORT).show();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public void onClick(RecyclerView recyclerView, ScrambleAdapter adapter, RecyclerView.ViewHolder viewHolder, View view) {
+            MergeRecyclerAdapter.LocalAdapter la = mMergeRecyclerAdapter.getAdapterOffsetForItem(viewHolder.getAdapterPosition());
+            if (la.mAdapter == mItemAdapter) {
+                if (viewHolder instanceof TextInfoViewHolderFactory.ViewHolder) {
+                    TextInfoViewHolderFactory.ViewHolder vh = (TextInfoViewHolderFactory.ViewHolder) viewHolder;
+                    Snackbar.make(view, "Action button pressed : " + vh.editText.getText(), Snackbar.LENGTH_SHORT).show();
+                }
             }
         }
     };
@@ -67,21 +113,30 @@ public class OperatableListExampleActivity extends AppCompatActivity {
         // find views
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler);
 
-        mMergeRecyclerAdapter = new MergeRecyclerAdapter(this);
+        mMergeRecyclerAdapter = new MergeRecyclerAdapter<>(this);
         {
             mSimpleStringAdapter = new SimpleStringAdapter(this, Collections.singletonList("This is header"));
             mMergeRecyclerAdapter.addAdapter(mSimpleStringAdapter);
         }
         {
             List<Object> items = new ArrayList<>();
-            for (int i = 0; i < 10; i++) {
-                items.add(new MyInfo());
+            for (int i = 0; i < 30; i++) {
+                switch(i % 2) {
+                    case 0:
+                        items.add(new MyInfo());
+                        break;
+                    case 1:
+                        items.add(new TextInfo());
+                        break;
+                }
             }
-            mItemAdapter = new ScrambleAdapter(this, items, mListenerRelay, new MyInfoViewHolderFactory());
+            mItemAdapter = new ScrambleAdapter(this, items, mListenerRelay,
+                    new MyInfoViewHolderFactory(),
+                    new TextInfoViewHolderFactory()
+            );
             mMergeRecyclerAdapter.addAdapter(mItemAdapter);
         }
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         mRecyclerView.setAdapter(mMergeRecyclerAdapter);
     }
-
 }
---
+++ example/src/main/java/net/cattaka/android/snippets/example/data/TextInfo.java
@@ -0,0 +1,16 @@
+package net.cattaka.android.snippets.example.data;
+
+/**
+ * Created by cattaka on 16/05/15.
+ */
+public class TextInfo {
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
